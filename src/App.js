d1eac4efec9e2b9c624feb3d5fcbb0de1a5e26b2
Luong-Anh99 <khimalata@gmail.com>
2021-08-23 10:23:41 +0700

    Them header cho trang web, link cho các thành phần trong header

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 
 const HatsPage=()=>(
@@ -18,6 +19,7 @@ const HatsPage=()=>(
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route  path='/shop' component={ShopPage} />
