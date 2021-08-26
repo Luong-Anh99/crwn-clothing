f1f06f205901743dcb342fad3a7b0d920a97b395
Luong-Anh99 <khimalata@gmail.com>
2021-08-26 19:33:39 +0700

    Thêm chức năng tăng giảm số lượng sản phẩm tại checkout item component

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ class App extends React.Component {
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/checkout' component={CheckOutPage} />
-          <Route 
+          <Route  
             path='/signin' 
             render={()=>
               this.props.currentUser  ? (
